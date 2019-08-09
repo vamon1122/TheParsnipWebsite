0d8ec43a518d5e255b84ac8911dcf48bcaadaa6a
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-08-09 15:34:55 +0100

    Rename Part 2

---
+++ ParsnipWebsite/Logout.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ParsnipData.Accounts;
+using ParsnipData.Logs;
+
+namespace ParsnipWebsite
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Data.DeviceType) || string.IsNullOrWhiteSpace(Data.DeviceType))
+                Response.Redirect("get_device_info?url=logout");
+
+            User myUser = new User("logout get name");
+            myUser.LogIn();
+            new LogEntry(new Log("login/out")) { text = String.Format("{0} logged out from {1} {2} device.", myUser.FullName, myUser.PosessivePronoun, Data.DeviceType) };
+            ParsnipData.Accounts.User.LogOut();
+            Response.Redirect("login");
+        }
+    }
+}
