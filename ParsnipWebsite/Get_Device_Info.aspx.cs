96cfeb2a434657a490158ff45b72dcf32f3d307d
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2020-05-16 22:45:39 +0100

    Add OG tags to /me #106

--- ParsnipWebsite/Get_Device_Info.aspx.cs
+++ ParsnipWebsite/Get_Device_Info.aspx.cs
@@ -12,9 +12,21 @@ namespace ParsnipWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["url"] != null)
+            {
+                if (Request.QueryString["url"] == "me")
+                {
+                    Page.Header.Controls.Add(new LiteralControl("<meta property=\"og:title\" content=\"What DIRT do we have on YOU? 😜\" />"));
+                    Page.Header.Controls.Add(new LiteralControl($"<meta property=\"og:image\" content=\"{Request.Url.GetLeftPart(UriPartial.Authority)}/Resources/Media/Images/Local/Dirt_On_You.png\" />"));
+                    Page.Header.Controls.Add(new LiteralControl("<meta property=\"og:type\" content=\"website\" />"));
+                    Page.Header.Controls.Add(new LiteralControl($"<meta property=\"og:url\" content=\"{Request.Url.ToString()}\" />"));
+                    Page.Header.Controls.Add(new LiteralControl("<meta property=\"og:description\" content=\"What DIRT do we have on YOU? 😜\" />"));
+                    Page.Header.Controls.Add(new LiteralControl("<meta property=\"og:alt\" content=\"What DIRT do we have on YOU? 😜\" />"));
+                    Page.Header.Controls.Add(new LiteralControl("<meta property=\"fb:app_id\" content=\"521313871968697\" />"));
+                }
+            }
             System.Diagnostics.Debug.WriteLine("Writing sessionId cookie = " + Session.SessionID.ToString());
             Cookie.WriteSession("sessionId", Session.SessionID.ToString());
-
         }
     }
 }
