72d47e2fe49a57fcf1fcfd964d8b4df981bfe8af
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2020-05-16 22:18:48 +0100

    Add /me page which redirects to user's tag #102

---
+++ ParsnipWebsite/Me.aspx.cs
@@ -0,0 +1,29 @@
+﻿using ParsnipData.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ParsnipData.Media;
+using ParsnipData.Logging;
+
+namespace ParsnipWebsite
+{
+    public partial class Me : System.Web.UI.Page
+    {
+        User myUser;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Data.DeviceType))
+                Response.Redirect("get_device_info?url=me");
+
+            myUser = Account.SecurePage("me", this, Data.DeviceType);
+
+            if (myUser != null)
+                Response.Redirect($"tag?user={myUser.Id}");
+            else
+                Response.Redirect("home");
+        }
+    }
+}
